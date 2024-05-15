0177da3488dfd9e56210f8491eebeedfae02300d
Trialsoftware <trailsoftware@gmail.com>
2024-05-15 11:03:51 -0700

    first code for cash

---
+++ cash.java
@@ -0,0 +1,20 @@
+package OrderManagement;
+
+
+/**
+ * @author Hope
+ * @version 1.0
+ * @created 29-Apr-2024 4:12:26 PM
+ */
+public class Cash extends Payment {
+
+	private float CashTendered;
+
+	public Cash(){
+
+	}
+
+	public void finalize() throws Throwable {
+		super.finalize();
+	}
+}//end Cash
