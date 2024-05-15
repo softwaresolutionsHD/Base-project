4c01b54b57c651c8460972d2d29dfc2aa92edcd1
Trialsoftware <trailsoftware@gmail.com>
2024-05-15 11:06:08 -0700

    modified version of cash

--- cash.java
+++ cash.java
@@ -18,3 +18,25 @@ public class Cash extends Payment {
 		super.finalize();
 	}
 }//end Cash
+package OrderManagement;
+
+
+/**
+ * @author Hope
+ * @version 1.0
+ * @created 29-Apr-2024 4:12:26 PM
+ */
+public class Customer {
+
+	private int address;
+	private String name;
+	public Order m_Order;
+
+	public Customer(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+}//end Customer
